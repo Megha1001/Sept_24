1e6a5097f1cd3254e019fe9674f83003af9557a0
Megha Verma <meghaverma1001@gmail.com>
2024-09-04 17:15:01 +0530

    Adding countOfDigits code

---
+++ src/mathematics/CountOfDigits.java
@@ -0,0 +1,27 @@
+package mathematics;
+
+/*
+ * Idea is to remove last digit in every iteration by dividing by 10 and update the input and do a count
+ */
+
+public class CountOfDigits{
+
+
+    public static void main(String args[]){
+        int input = 9345;
+        System.out.println("Number of digits in input is : "+countNumOfDigits(input));
+    }
+
+
+    public static int countNumOfDigits(int n){
+        int count = 0;
+
+        while(n>0){
+            ++count;
+            n /= 10;
+        }
+
+        return count;
+    }
+
+}
