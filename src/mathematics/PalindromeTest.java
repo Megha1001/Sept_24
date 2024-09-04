73ce07905df83d1924070cb1f6aa8b273a2ac779
Megha Verma <meghaverma1001@gmail.com>
2024-09-04 17:37:11 +0530

    Create PalindromeTest.java

---
+++ src/mathematics/PalindromeTest.java
@@ -0,0 +1,26 @@
+package mathematics;
+
+/*
+ * Idea is to find the reverse of the number and compare with original
+ */
+
+class PalindromeTest {
+
+    public static void main(String args[]){
+        int input = 78987;
+        System.out.println("Is the given number palindrome ? "+isPalindrome(input));
+    }
+
+    public static boolean isPalindrome(int n){
+        int temp = n;
+        int rev = 0;
+
+        while(temp>0){
+            rev = rev*10 + (temp%10); //add last digit
+            temp /= 10;
+        }
+
+        return n==rev;
+    }
+    
+}
