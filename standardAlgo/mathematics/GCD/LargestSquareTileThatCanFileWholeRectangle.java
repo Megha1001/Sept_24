275d649e854087800e417c328ce6c1f0a5b58e2a
Megha Verma <meghaverma1001@gmail.com>
2024-09-05 07:34:24 +0530

    Update LargestSquareTileThatCanFileWholeRectangle.java

--- standardAlgo/mathematics/GCD/LargestSquareTileThatCanFileWholeRectangle.java
+++ standardAlgo/mathematics/GCD/LargestSquareTileThatCanFileWholeRectangle.java
@@ -1,3 +1,33 @@
 /*
  * Tiling Problem : If we have two numbers a, b and if we make a rectangle of size a*b, then our GCD problem is finding sides of the largest square using which we can tile the whole rectangle.
+ * USE : EUCLIDIEN ALGO
+ * TIME Complexity : O(N)
+ * Auxilary Space : Theta(1)
  */
+
+ package standardAlgo.mathematics.GCD;
+
+ public class LargestSquareTileThatCanFileWholeRectangle{
+
+    public static void main(String args[]){
+        int a = 4;
+        int b = 6;
+
+        System.out.println("GCD of given number is : "+findGCDUsingEuclidienAlgo(a,b));
+    }
+
+
+    public static int findGCDUsingEuclidienAlgo(int a, int b){
+        
+        while(a!=b){
+            if(a>b){
+                a = a-b;
+            }else{
+                b = b-a;
+            }
+        }
+
+        return a; //we can return any a or b
+    }
+
+ }
