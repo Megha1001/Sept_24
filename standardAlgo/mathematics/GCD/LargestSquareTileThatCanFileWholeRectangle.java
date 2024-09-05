bf923c24a0f89023b08e8dea1a7ef91f15f5b041
Megha Verma <meghaverma1001@gmail.com>
2024-09-05 07:16:04 +0530

    Create LargestSquareTileThatCanFileWholeRectangle.java

---
+++ standardAlgo/mathematics/GCD/LargestSquareTileThatCanFileWholeRectangle.java
@@ -0,0 +1,3 @@
+/*
+ * Tiling Problem : If we have two numbers a, b and if we make a rectangle of size a*b, then our GCD problem is finding sides of the largest square using which we can tile the whole rectangle.
+ */
